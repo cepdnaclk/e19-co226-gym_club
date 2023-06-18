48c2af2bb4ba6d1e0a3f9c7265314c673254b83c
CColombus <115539695+CColombus@users.noreply.github.com>
2023-06-18 13:28:32 +0530

    Setup the database

--- SQL/user_form.sql
+++ SQL/user_form.sql
@@ -7,10 +7,12 @@
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
+CREATE DATABASE IF NOT EXISTS gym;
+USE gym;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,50 +20,24 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `gym`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user_form`
 --
-
-CREATE TABLE `user_form` (
-  `id` int(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user_form` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `user_type` enum('user','trainer') NOT NULL
+  `user_type` enum('user','trainer') NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_form`
 --
-
 INSERT INTO `user_form` (`id`, `name`, `email`, `password`, `user_type`) VALUES
 (1, 'Pramuditha', 'def@gmail.com', '1ae2a53ad24da29123bf211c892a387d', 'user'),
 (2, 'Vidura', 'abc@gmail.com', '827f76d04c7ee686dc84310168e28f5f', 'user');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `user_form`
---
-ALTER TABLE `user_form`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `user_form`
---
-ALTER TABLE `user_form`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
